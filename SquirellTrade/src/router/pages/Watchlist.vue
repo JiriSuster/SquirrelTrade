6e9a5e67f359569eb9221af5357d9ca7a3eeb34f
David Krcmar <xkrcmar2@mendelu.cz>
2024-06-12 23:45:54 +0200

    watch list design

--- SquirellTrade/src/router/pages/Watchlist.vue
+++ SquirellTrade/src/router/pages/Watchlist.vue
@@ -22,7 +22,40 @@ const watchStore = useWatchlistStore();
       </div>
 
       <h1>Watchlist</h1>
+      <div>
+        <v-list-item class="custom-list-item">
+          <div class="d-flex justify-space-around">
+            <v-col class="d-flex justify-center" cols="2">Name</v-col>
+            <v-col class="d-flex justify-center" cols="2">Close</v-col>
+            <v-col class="d-flex justify-center" cols="2">Change</v-col>
+            <v-col class="d-flex justify-center" cols="2">High</v-col>
+            <v-col class="d-flex justify-center" cols="2">Low</v-col>
+            <v-col class="d-flex justify-center" cols="2">Unwatch</v-col>
+          </div>
+        </v-list-item>
 
+        <v-list lines="one">
+          <v-list-item class="custom-list-item"
+              v-for="(symbol,index) in watchStore.favoriteSymbols"
+              :key="index"
+          >
+            <div class="d-flex justify-space-between align-center pa-0">
+              <v-col class="d-flex justify-center pa-0" cols="2">neco</v-col>
+              <v-col class="d-flex justify-center pa-0" cols="2">neco</v-col>
+              <v-col class="d-flex justify-center pa-0" cols="2">neco</v-col>
+              <v-col class="d-flex justify-center pa-0" cols="2">neco</v-col>
+              <v-col class="d-flex justify-center pa-0" cols="2">neco</v-col>
+              <v-col class="d-flex justify-center pa-0" cols="2">
+                <v-btn color="yellow-darken-2"  size="small" density="default" class="ma-1 delete-btn" rounded="xl" @click="watchStore.toggleFavorite(symbol)">
+                  Delete
+                </v-btn>
+              </v-col>
+
+
+            </div>
+          </v-list-item>
+        </v-list>
+      </div>
 
 
 
@@ -32,4 +65,14 @@ const watchStore = useWatchlistStore();
 
 <style scoped>
 
+.delete-btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.custom-list-item {
+  background-color: #f0f0f0;
+  border-radius: 10px;
+}
 </style>
